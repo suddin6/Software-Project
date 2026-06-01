54d189162850f5af42e6b11ae5630e257d915b30
Sumaya Uddin <suddin6@oakland.edu>
2026-06-01 14:08:55 -0400

    comments added to CampaignController.java for organization and readability

--- src/main/java/com/example/votingveranda/CampaignController.java
+++ src/main/java/com/example/votingveranda/CampaignController.java
@@ -5,18 +5,23 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class CampaignController {
+    // information to connect to database using mySQL workbench
     private final String DB_URL = "jdbc:mysql://localhost:3306/voting_veranda";
     private final String DB_USER = "root";
     private final String DB_PASS = "root";
     private java.sql.Connection conn = null;
 
+    // hard-coded user to display on candidate page
     private int currentUser = 1;
+
+    // fx:ids from .fxml file
     @FXML private Label campaignText;
     @FXML private Label candidateName;
 
+    // database connection method
     private void db_connection() {
+        // if "Connecting" and "Success" show up in terminal, database has connected
         System.out.println("Connecting");
-
         try {
             conn = java.sql.DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
             if (conn != null) {
@@ -30,13 +35,14 @@ public class CampaignController {
     }
 
     @FXML
-
+    // method used to initialize the candidate page
     public void initialize() {
         db_connection();
         loadCampaign(currentUser);
         viewStandings(currentUser);
     }
 
+    // display the campaign on the candidate page
     private void loadCampaign(int candidateID) {
         if (conn == null) {
             campaignText.setText("Not shown yet");
@@ -44,6 +50,7 @@ public class CampaignController {
             return;
         }
 
+        // using an SQL query to display the campaign and candidate name
         try {
             String query = "SELECT l.first_name, l.last_name, c.campaign FROM candidate c " +
                             "INNER JOIN login l ON c.login_id = l.login_id " +
